217f3fe038183a926180297bbffb0c1f154000a5
Christopher Hill <cjameshill@gmail.com>
2023-03-07 09:30:01 +0000

    Update 4 files

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,10 @@
 import { createApp } from "vue";
-import axios from "axios";
-import VueAxios from "vue-axios";
 import "./style.css";
 import App from "./App.vue";
 
 import router from "./router";
 
 const app = createApp(App);
-app.use(VueAxios, axios);
-app.provide("axios", app.config.globalProperties.axios);
-
 app.use(router);
 
 app.mount("#app");
